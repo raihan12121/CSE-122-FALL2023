51563ab57fab2e3956d639941e890c0fa6580f50
Muhammad Raihan Molla <mdraihan2328@gmail.com>
2023-08-22 18:33:25 +0600

    Add files via upload

---
+++ Lab_02/function overloading.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+
+    void sum(int x)
+    {
+       int add= x;
+       cout<<add<<endl;
+
+    }
+
+  void sum(double x)
+    {
+       int add= x;
+       cout<<add<<endl;
+
+    }
+       int main()
+{
+    sum(12);
+    sum(10.30);
+    getch();
+}
